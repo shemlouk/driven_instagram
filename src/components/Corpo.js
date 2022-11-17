fad82c55b648333d3bf580da353f9325d3d473ad
samuel <shemlouk@gmail.com>
2022-11-16 22:54:05 -0300

    feat: fragmentação da página

---
+++ src/components/Corpo.js
@@ -0,0 +1,16 @@
+export default function Corpo() {
+  return (
+    <div class="corpo">
+      <div class="esquerda"></div>
+
+      <div class="sidebar">
+        <div class="links">
+          Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos •
+          Localizações • Contas mais relevantes • Hashtags • Idioma
+        </div>
+
+        <div class="copyright">© 2021 INSTAGRAM DO FACEBOOK</div>
+      </div>
+    </div>
+  );
+}
