81c12282a0be6c3ac16ad5e62073f490d8c886d4
samuel <shemlouk@gmail.com>
2022-11-19 15:03:59 -0300

    feat: adicionado função de alterar nome e imagem de usuário

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,14 +1,43 @@
+import { useState } from "react";
+
 export default function Usuario() {
+  const [name, setName] = useState("Catana");
+  const [image, setImage] = useState("assets/img/catanacomics.svg");
   return (
     <div className="usuario">
-      <img data-user="image" src="assets/img/catanacomics.svg" />
+      <img
+        onClick={() => setImage(changeProfile(image, "image"))}
+        src={image}
+      />
       <div className="texto">
         <strong>catanacomics</strong>
-        <span data-user="name">
-          Catana
-          <ion-icon name="pencil"></ion-icon>
+        <span>
+          {name}
+          <ion-icon
+            onClick={() => setName(changeProfile(name, "name"))}
+            name="pencil"
+          ></ion-icon>
         </span>
       </div>
     </div>
   );
+}
+
+function changeProfile(defaultValue, type) {
+  const msgs = {
+    name: {
+      get: "Qual o seu nome?",
+      invalid: "Nome inválido!",
+    },
+    image: {
+      get: "Qual a URL da imagem?",
+      invalid: "URL inválida!",
+    },
+  };
+  const newValue = prompt(msgs[type].get);
+  if (!newValue) {
+    alert(msgs[type].invalid);
+    return defaultValue;
+  }
+  return newValue;
 }
